b486a2dd2fb1554af3e5cd12866f82afe4335024
wida <wida@foxmail.com>
2018-10-21 21:19:20 +0800

    first commit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/widaT/damogo/pb"
 	"context"
 	"google.golang.org/grpc"
+	"fmt"
 )
 
 const (
@@ -18,9 +19,18 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewSearcherClient(conn)
+	c := pb.NewFacedbClient(conn)
 
-	r, err := c.Search(context.Background(), &pb.SearchRequest{Name: []float32{111}})
+
+	ret, err := c.AddUser(context.Background(),&pb.UserInfo{Id:"wida",Feature:&pb.Feature{Feature:[]float32{1.0,33.9}}})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	//log.Printf("Greeting: %s", r.Users)
+	fmt.Println(ret)
+
+
+	r, err := c.Search(context.Background(), &pb.SearchRequest{Group:"wida",Feature:[]float32{111}})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}

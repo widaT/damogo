fa4a1e28bfd2442cdd1fc04d03e949565103409a
wida <wida@foxmail.com>
2018-11-05 20:00:37 +0800

    damo golang client

--- pkg/utils/etcdclient_test.go
+++ pkg/utils/etcdclient_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestEtcdSetDefaultHost(t *testing.T) {
 
-	//err := EtcdSetDefaultHost("172.30.60.8:8009||0||{")
-	err := EtcdSetDefaultHost("127.0.0.1:50051||0||{")
+	err := EtcdSetDefaultHost("172.30.60.8:8009||0||{")
+	//err := EtcdSetDefaultHost("127.0.0.1:50051||0||{")
 	if err != nil{
 		t.Fatal(err)
 	}
